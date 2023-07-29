4b1d503b54eed2a1f5ca5ae1d5943f01e1fffd58
Williams E. Fernández <williamsenrique@gmail.com>
2023-07-29 10:26:26 -0600

    Agregando pyoyecto de API para TODOs - Estructura de tablas - API REST con métodos Save, GetAll y GetById

---
+++ src/main/java/com/learning/todoapi/controllers/TodoController.java
@@ -0,0 +1,35 @@
+package com.learning.todoapi.controllers;
+
+import com.learning.todoapi.entities.Todo;
+import com.learning.todoapi.entities.TodoRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoRepository todoRepository;
+
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @GetMapping
+    public List<Todo> getTodos() {
+        return todoRepository.findAll();
+    }
+
+    @GetMapping("/{todoId}")
+    public Optional<Todo> getTodo(@PathVariable("todoId") Long todoId) {
+        var todo = todoRepository.findById(todoId);
+        return todo;
+    }
+
+    @PostMapping
+    public Todo newTodo(@RequestBody Todo todo) {
+        return this.todoRepository.save(todo);
+    }
+}
